a4e76728bc8d898b35a64d11d8179d23b77360f6
roberty99 <48930705+roberty99@users.noreply.github.com>
2025-03-28 15:00:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyephember2',
-    version='0.4.9',
+    version='0.4.10',
     description='Python library to work with ember from EPH Controls',
     keywords='ephember',
     author='Robert Young',
